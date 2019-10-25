3284b6222bb6fcef8be9782d9acbde5b1a0f55fe
Tran Quang Tan <tantq@aureole.local>
2019-10-25 17:11:08 +0700

    first

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,38 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Cookies from 'js-cookie'
+import Element from 'element-ui'
+// import filters from './filters' // global filters
+
+import '@/styles/index.scss' // global css
+
+import './icons' // icon
+// import './permission' // permission control
+import './utils/error-log' // error log
+
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online! ! !
+ */
+import { mockXHR } from '../mock'
+if (process.env.NODE_ENV === 'production') {
+  mockXHR()
+}
+
+Vue.use(Element, {
+    size: Cookies.get('size') || 'medium' // set element-ui default size
+  })
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,22 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Layout from '../layout/index.vue'
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [
+        {
+          path: 'dashboard',
+          component: () => import('../views/dashboard/index.vue'),
+          name: 'Dashboard',
+          meta: { title: 'Dashboard', icon: 'dashboard', affix: true }
+        }
+      ] 
   }
 ];
 
